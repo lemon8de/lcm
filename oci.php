90231a2401090b3f6b826dead9eebb3b0465c329
lemon8de <jayandrew.minon@gmail.com>
2024-08-27 11:38:44 +0800

    test

---
+++ oci.php
@@ -0,0 +1,14 @@
+<?php
+$username = "ircs";
+$password = "ircs";
+$database = "172.25.119.1:1521/fsib";
+$conn_ircs = oci_connect($username, $password, $database);
+if (!$conn_ircs) {
+    $e = oci_error();
+    trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
+}
+
+echo <<<HTML
+    <h1>ALL GOOD</h1>
+HTML;
+?>
