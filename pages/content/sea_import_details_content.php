0bba0e33a1f0d67b6b2151930ea5bab4af57f25c
lemon8de <jayandrew.minon@gmail.com>
2024-09-06 17:01:00 +0800

    import report searching parameters

--- pages/content/sea_import_details_content.php
+++ pages/content/sea_import_details_content.php
@@ -18,6 +18,42 @@
                 </form>
             </button>
         </div>
+        <form id="ImportReportSearchForm">
+        <div class="container mb-2">
+            <div class="row">
+                    <div class="col-3">
+                        <select class="form-control" name="month" onchange="search_import_report()">
+                            <option value="" selected disabled>Select Month</option>
+                            <option value="1">January</option>
+                            <option value="2">February</option>
+                            <option value="3">March</option>
+                            <option value="4">April</option>
+                            <option value="5">May</option>
+                            <option value="6">June</option>
+                            <option value="7">July</option>
+                            <option value="8">August</option>
+                            <option value="9">September</option>
+                            <option value="10">October</option>
+                            <option value="11">November</option>
+                            <option value="12">December</option>
+                        </select>
+                    </div>
+                    <div class="col-2">
+                        <select class="form-control" name="year" onchange="search_import_report()">
+                            <?php
+                                $current_year = date("Y");
+                                $end_year = $current_year - 10;
+                                for ($year = $current_year; $year >= $end_year; $year--) {
+                                    echo <<<HTML
+                                        <option value="{$year}">{$year}</option>
+                                    HTML;
+                                }
+                            ?>
+                        </select>
+                    </div>
+            </div>
+        </div>
+        </form>
         <div class="container" style="max-height: 600px; overflow-y: auto;" id="ImportDataMain">
             <?php include '../php_static/content_tables/importsea_data_report.php';?>
         </div>
@@ -99,4 +135,20 @@
         document.getElementById('ImportDataMain').style.display = 'none';
         document.getElementById('ContainerBreakdownSwitch').style.display = 'block';
     }
+
+    function search_import_report() {
+        var formData = $('#ImportReportSearchForm').serialize();
+        $.ajax({
+            type: 'GET',
+            url: '../php_api/refine_import_report.php',
+            data: formData,
+            dataType: 'json',
+            success: function(response) {
+                if (!response.exited) {
+                    document.getElementById("ImportReportContent").innerHTML = response.inner_html;
+                }
+            },
+        });
+    }
+
 </script>
