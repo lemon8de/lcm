641259934f11ee63f1f6a6039c0e2e0f6a5bdb7d
lemon8de <jayandrew.minon@gmail.com>
2024-09-19 17:31:11 +0800

    asdf

--- php_api/import_outgoing_shipment.php
+++ php_api/import_outgoing_shipment.php
@@ -3,7 +3,7 @@
     require '../php_static/session_lookup.php';
     $csvMimes = array('text/x-comma-separated-values', 'text/comma-separated-values', 'application/octet-stream', 'application/vnd.ms-excel', 'application/x-csv', 'text/x-csv', 'text/csv', 'application/csv', 'application/excel', 'application/vnd.msexcel', 'text/plain');
 
-    echo 'made it here';
+    echo 'made it here11';
 
     if (!empty($_FILES['outgoing_shipment_file']['name']) && in_array($_FILES['outgoing_shipment_file']['type'],$csvMimes)) {
         if (is_uploaded_file($_FILES['outgoing_shipment_file']['tmp_name'])) {
