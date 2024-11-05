c3556405b576907c0159a9cb3c2ece12253daef2
lemon8de <jayandrew.minon@gmail.com>
2024-11-05 14:16:58 +0800

    total sales on outgoing invoice

--- php_api/report_outgoing_invoice.php
+++ php_api/report_outgoing_invoice.php
@@ -13,7 +13,7 @@ $stmt -> execute();
 $inner_html = "";
 $count = 0;
 while ($data = $stmt -> fetch(PDO::FETCH_ASSOC)) {
-    $count++;
+    $count += $data['invoice_amount'];
     $inner_html .= <<<HTML
         <tr>
             <td>{$data['invoice_no']}</td>
@@ -26,10 +26,11 @@ while ($data = $stmt -> fetch(PDO::FETCH_ASSOC)) {
         </tr>
     HTML;
 }
+$count = number_format($count);
 $inner_html_count = <<<HTML
     <div class="ml-1">
         <div class="bg-success pl-4 pr-4" style="border-radius:.350rem;padding:0rem .350rem">
-            <h4 style="font-weight:700;line-height:1.5;">{$count}<span style="font-size:75%;font-weight:500;">&nbsp;Invoices</span></h4>
+            <h4 style="font-weight:700;line-height:1.5;">{$count}<span style="font-size:75%;font-weight:500;">&nbsp;JYP</span></h4>
         </div>
     </div>
 HTML;
