0d13de98b03f8620006dad5390a6642d4dfb4e91
lemon8de <jayandrew.minon@gmail.com>
2024-11-19 10:26:53 +0800

    image file deletion on deletion

--- php_api/delete_billing_forwarder.php
+++ php_api/delete_billing_forwarder.php
@@ -4,6 +4,19 @@
 
     $billing_forwarder_details_ref = $_POST['billing_forwarder_details_ref'];
 
+    $sql = "SELECT forwarder_logo from m_billing_forwarder where billing_forwarder_details_ref = :billing_forwarder_details_ref";
+    $stmt = $conn -> prepare($sql);
+    $stmt -> bindParam(":billing_forwarder_details_ref", $billing_forwarder_details_ref);
+    $stmt -> execute();
+    if ($data = $stmt -> fetch(PDO::FETCH_ASSOC)) {
+        $filePath = $data['forwarder_logo'];
+        // Check if the file exists
+        if (file_exists($filePath)) {
+            // Attempt to delete the file
+            unlink($filePath);
+        }
+    }
+
     $sql = "DELETE from m_billing_forwarder where billing_forwarder_details_ref = :billing_forwarder_details_ref;
             DELETE from m_billing_compute where billing_forwarder_details_ref = :billing_forwarder_details_ref2;
     ";
