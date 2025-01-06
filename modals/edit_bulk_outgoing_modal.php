49085fc69f7c6fba7b5770dd24338d7868cbf862
vicento-falp <vince.dale.alcantara@furukawaelectric.com>
2025-01-06 13:40:57 +0800

    new async call + api

--- modals/edit_bulk_outgoing_modal.php
+++ modals/edit_bulk_outgoing_modal.php
@@ -351,6 +351,7 @@
         document.getElementById('OutgoingVesselFormb').reset();
         document.getElementById('OutgoingInvoiceDetailsFormb').reset();
         document.getElementById('OutgoingContainerDetailsFormb').reset();
+        document.getElementById('OutgoingDispatchingDetailsFormb').reset();
     });
     $('#OutgoingFSIBFormb').on('submit', function(event) {
         event.preventDefault(); // Prevent the default form submission
@@ -431,6 +432,29 @@
         $.ajax({
             type: 'POST', // or 'GET' depending on your needs
             url: '../php_api/outgoing_bulk_edits/container.php', // Replace with your server endpoint
+            data: combinedData,
+            dataType: 'json',
+            success: function(response) {
+                if (response.notification) {
+                    Toast.fire({
+		                icon: response.notification.icon,
+		                title: response.notification.text,
+	                })
+                }
+            },
+        });
+    });
+
+    $('#OutgoingDispatchingDetailsFormb').on('submit', function(event) {
+        event.preventDefault(); // Prevent the default form submission
+        // Serialize the form data
+        var formData = $(this).serialize();
+        var additionalData = $.param({ 'outgoing_details_ref': selectedIds });
+        var combinedData = formData + '&' + additionalData;
+        // Send the AJAX request
+        $.ajax({
+            type: 'POST', // or 'GET' depending on your needs
+            url: '../php_api/outgoing_bulk_edits/dispatch.php', // Replace with your server endpoint
             data: combinedData,
             dataType: 'json',
             success: function(response) {
