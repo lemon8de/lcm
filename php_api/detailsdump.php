d7da842e611912ba8130b2d45e91c78d5e2621b5
vicento-falp <vince.dale.alcantara@furukawaelectric.com>
2025-01-27 08:14:09 +0800

    moved shipment details up, becuase of new LCL type shipment lock on the rest of the forms

--- php_api/detailsdump.php
+++ php_api/detailsdump.php
@@ -3,170 +3,6 @@
     $shipment_details_ref = $_GET['shipment_details_ref'];
     $return_body = [];
 
-    //delivery_plan
-    $sql = "SELECT required_delivery_sched, deliver_plan, tabs from m_delivery_plan where shipment_details_ref = :shipment_details_ref";
-    $stmt = $conn -> prepare($sql);
-    $stmt -> bindValue(':shipment_details_ref', $shipment_details_ref);
-    $stmt -> execute();
-    $shipment = $stmt->fetch(PDO::FETCH_ASSOC);
-
-    if ($shipment) {
-        $shipment['required_delivery_sched'] = $shipment['required_delivery_sched'] == null ? null : substr($shipment['required_delivery_sched'], 0, 10);
-        $shipment['deliver_plan'] = $shipment['deliver_plan'] == null ? null : substr($shipment['deliver_plan'], 0, 10);
-        $shipment['tabs'] = $shipment['tabs'] == null ? null : substr($shipment['tabs'], 0, 5);
-        $return_body['delivery_plan'] = <<<HTML
-            <input readonly style="display:none;" value="{$shipment_details_ref}" type="text" name="shipment_details_ref">
-            <div class="row mb-2">
-                <div class="col-6">
-                    <label>REQUIRED DELIVERY SCHEDULE</label>
-                    <input value="{$shipment['required_delivery_sched']}" type="date" class="form-control" name="required_delivery_sched">
-                </div>
-                <div class="col-6">
-                    <label>DELIVERY PLAN</label>
-                    <input value="{$shipment['deliver_plan']}" type="date" class="form-control" name="deliver_plan">
-                </div>
-            </div>
-            <div class="row mb-2">
-                <div class="col-6">
-                    <label>TABS</label>
-                    <input value="{$shipment['tabs']}" type="text" class="form-control" name="tabs">
-                </div>
-            </div>
-            <div class="row mb-2 d-flex align-items-center">
-                <div class="col-3 ml-auto">
-                    <button type="submit" class="btn bg-primary btn-block">Update</button>
-                </div>
-            </div>
-        HTML;
-    }
-
-    //completion details
-    $sql = "SELECT date_port_out, actual_received_at_falp from m_completion_details where shipment_details_ref = :shipment_details_ref"; 
-    $stmt = $conn -> prepare($sql);
-    $stmt -> bindValue(':shipment_details_ref', $shipment_details_ref);
-    $stmt -> execute();
-    $shipment = $stmt->fetch(PDO::FETCH_ASSOC);
-
-    if ($shipment) {
-        $shipment['date_port_out'] = $shipment['date_port_out'] == null ? null : substr($shipment['date_port_out'], 0, 10);
-        $shipment['actual_received_at_falp'] = $shipment['actual_received_at_falp'] == null ? null : substr($shipment['actual_received_at_falp'], 0, 10);
-        $return_body['completion_details'] = <<<HTML
-            <input readonly style="display:none;" value="{$shipment_details_ref}" type="text" name="shipment_details_ref">
-            <div class="row mb-2">
-                <div class="col-6">
-                    <label>DATE PORT OUT</label>
-                    <input type="date" value="{$shipment['date_port_out']}" class="form-control" name="date_port_out">
-                </div>
-                <div class="col-6">
-                    <label>ACTUAL RECEIVED AT FALP</label>
-                    <input type="date" value="{$shipment['actual_received_at_falp']}" class="form-control" name="actual_received_at_falp">
-                </div>
-            </div>
-            <div class="row mb-2 d-flex align-items-center">
-                <div class="col-3 ml-auto">
-                    <button type="submit" class="btn bg-primary btn-block">Update</button>
-                </div>
-            </div>
-        HTML;
-    }
-
-    //polytainer details
-    $sql = "SELECT polytainer_size, polytainer_quantity, etd from m_polytainer_details where shipment_details_ref = :shipment_details_ref";
-    $stmt = $conn -> prepare($sql);
-    $stmt -> bindValue(':shipment_details_ref', $shipment_details_ref);
-    $stmt -> execute();
-    $shipment = $stmt->fetch(PDO::FETCH_ASSOC);
-
-    if ($shipment) {
-        $shipment['etd'] = $shipment['etd'] == null ? null : substr($shipment['etd'], 0, 10);
-        $return_body['polytainer_details'] = <<<HTML
-            <input readonly style="display:none;" value="{$shipment_details_ref}" type="text" name="shipment_details_ref">
-            <div class="row mb-2">
-                <div class="col-6">
-                    <label>POLYTAINER SIZE</label>
-                    <input value="{$shipment['polytainer_size']}" type="text" class="form-control" name="polytainer_size">
-                </div>
-                <div class="col-6">
-                    <label>POLYTAINER QUANTITY</label>
-                    <input value="{$shipment['polytainer_quantity']}" type="text" class="form-control" name="polytainer_quantity">
-                </div>
-            </div>
-            <div class="row mb-2">
-                <div class="col-6">
-                    <label>ETD</label>
-                    <input value="{$shipment['etd']}" type="date" class="form-control" name="etd">
-                </div>
-            </div>
-            <div class="row mb-2 d-flex align-items-center">
-                <div class="col-3 ml-auto">
-                    <button type="submit" class="btn bg-primary btn-block">Update</button>
-                </div>
-            </div>
-        HTML;
-    }
-
-    //mm_system details
-    $sql = "SELECT container_status, date_return_reused, no_days_port, no_days_falp from m_mmsystem where shipment_details_ref = :shipment_details_ref";
-    $stmt = $conn -> prepare($sql);
-    $stmt -> bindValue(':shipment_details_ref', $shipment_details_ref);
-    $stmt -> execute();
-    $shipment = $stmt->fetch(PDO::FETCH_ASSOC);
-
-    if ($shipment) {
-        $shipment['date_return_reused'] = $shipment['date_return_reused'] == null ? null : substr($shipment['date_return_reused'], 0, 10);
-        $return_body['mmsystem_details'] = <<<HTML
-            <input readonly style="display:none;" value="{$shipment_details_ref}" type="text" name="shipment_details_ref">
-            <div class="row mb-2">
-                <div class="col-6">
-                    <label>CONTAINER STATUS</label>
-                    <input value="{$shipment['container_status']}" type="text" class="form-control" name="container_status">
-                </div>
-                <div class="col-6">
-                    <label>DATE RETURN / REUSED</label>
-                    <input value="{$shipment['date_return_reused']}" type="date" class="form-control" name="date_return_reused">
-                </div>
-            </div>
-            <div class="row mb-2">
-                <div class="col-6">
-                    <label>NO. DAYS AT PORT</label>
-                    <input readonly value="{$shipment['no_days_port']}" type="number" class="form-control" name="no_days_port">
-                </div>
-                <div class="col-6">
-                    <label>NO. DAYS AT FALP</label>
-                    <input readonly value="{$shipment['no_days_falp']}" type="number" class="form-control" name="no_days_falp">
-                </div>
-            </div>
-            <div class="row mb-2 d-flex align-items-center">
-                <div class="col-3 ml-auto">
-                    <button type="submit" class="btn bg-primary btn-block">Update</button>
-                </div>
-            </div>
-        HTML;
-    }
-
-    //history details
-    $sql = "SELECT username, column_name, changed_from, changed_to, date_modified from m_change_history where shipment_details_ref = :shipment_details_ref order by date_modified asc";
-    $stmt = $conn -> prepare($sql);
-    $stmt -> bindValue(':shipment_details_ref', $shipment_details_ref);
-    $stmt -> execute();
-
-    $return_body['history'] = null;
-    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-        $row['date_modified'] = date("Y/m/d", strtotime($row["date_modified"]));
-        $row['changed_from'] = $row['changed_from'] == null ? "N/A" : $row['changed_from'];
-        $row['changed_to'] = $row['changed_to'] == null ? "N/A" : $row['changed_to'];
-        $return_body['history'] .= <<<HTML
-            <tr>
-                <td>{$row['date_modified']}</td>
-                <td>{$row['username']}</td>
-                <td>{$row['column_name']}</td>
-                <td style="background-color: #ffcecb;">{$row['changed_from']}</td>
-                <td style="background-color: #d1f8d9;">{$row['changed_to']}</td>
-            </tr>
-        HTML;
-    }
-
-
     //shipment details
     $sql = "SELECT shipment_details_ref, bl_number, container, container_size, commercial_invoice, commodity, shipping_lines, forwarder_name, origin_port, destination_port, tsad_number, shipment_status, shipment_status_percentage from m_shipment_sea_details where shipment_details_ref = :shipment_details_ref";
     $stmt = $conn -> prepare($sql);
@@ -175,6 +11,8 @@
     $shipment = $stmt -> fetch(PDO::FETCH_ASSOC);
 
     if ($shipment) {
+        //revision check shipping line to close off some details
+        $shipping_line_lock = $shipment['shipping_lines'] == "LCL" ? "readonly" : "";
         $return_body['shipment'] = <<<HTML
             <input readonly style="display:none;" value="{$shipment['shipment_details_ref']}" type="text" name="shipment_details_ref">
             <div class="row mb-2">
@@ -244,7 +82,7 @@
                 <div class="col-6">
                     <label>SHIPPING LINES</label>
                     <!-- <input type="text" class="form-control" value="{$shipment['shipping_lines']}" name="shipping_lines" required> -->
-                    <select class="form-control" name="shipping_lines">
+                    <select class="form-control" name="shipping_lines" required>
                         <option value="" disabled selected>Shipping Line</option>
                         {$shipping_line_options} 
                     </select>
@@ -254,7 +92,7 @@
                 <div class="col-6">
                     <label>FORWARDER NAME</label>
                     <!-- <input type="text" class="form-control" value="{$shipment['forwarder_name']}" name="forwarder_name" required> -->
-                    <select class="form-control" name="forwarder_name">
+                    <select class="form-control" name="forwarder_name" required>
                         <option value="" disabled selected>Forwarder</option>
                         {$forwarder_options} 
                     </select>
@@ -291,6 +129,172 @@
             </div>
         HTML;
     }
+
+    //delivery_plan
+    $sql = "SELECT required_delivery_sched, deliver_plan, tabs from m_delivery_plan where shipment_details_ref = :shipment_details_ref";
+    $stmt = $conn -> prepare($sql);
+    $stmt -> bindValue(':shipment_details_ref', $shipment_details_ref);
+    $stmt -> execute();
+    $shipment = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    if ($shipment) {
+        $shipment['required_delivery_sched'] = $shipment['required_delivery_sched'] == null ? null : substr($shipment['required_delivery_sched'], 0, 10);
+        $shipment['deliver_plan'] = $shipment['deliver_plan'] == null ? null : substr($shipment['deliver_plan'], 0, 10);
+        $shipment['tabs'] = $shipment['tabs'] == null ? null : substr($shipment['tabs'], 0, 5);
+        $return_body['delivery_plan'] = <<<HTML
+            <input readonly style="display:none;" value="{$shipment_details_ref}" type="text" name="shipment_details_ref">
+            <div class="row mb-2">
+                <div class="col-6">
+                    <label>REQUIRED DELIVERY SCHEDULE</label>
+                    <input value="{$shipment['required_delivery_sched']}" type="date" class="form-control" name="required_delivery_sched">
+                </div>
+                <div class="col-6">
+                    <label>DELIVERY PLAN</label>
+                    <input value="{$shipment['deliver_plan']}" type="date" class="form-control" name="deliver_plan">
+                </div>
+            </div>
+            <div class="row mb-2">
+                <div class="col-6">
+                    <label>TABS</label>
+                    <input value="{$shipment['tabs']}" type="text" class="form-control" name="tabs" {$shipping_line_lock}>
+                </div>
+            </div>
+            <div class="row mb-2 d-flex align-items-center">
+                <div class="col-3 ml-auto">
+                    <button type="submit" class="btn bg-primary btn-block">Update</button>
+                </div>
+            </div>
+        HTML;
+    }
+
+    //completion details
+    $sql = "SELECT date_port_out, actual_received_at_falp from m_completion_details where shipment_details_ref = :shipment_details_ref"; 
+    $stmt = $conn -> prepare($sql);
+    $stmt -> bindValue(':shipment_details_ref', $shipment_details_ref);
+    $stmt -> execute();
+    $shipment = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    if ($shipment) {
+        $shipment['date_port_out'] = $shipment['date_port_out'] == null ? null : substr($shipment['date_port_out'], 0, 10);
+        $shipment['actual_received_at_falp'] = $shipment['actual_received_at_falp'] == null ? null : substr($shipment['actual_received_at_falp'], 0, 10);
+        $return_body['completion_details'] = <<<HTML
+            <input readonly style="display:none;" value="{$shipment_details_ref}" type="text" name="shipment_details_ref">
+            <div class="row mb-2">
+                <div class="col-6">
+                    <label>DATE PORT OUT</label>
+                    <input type="date" value="{$shipment['date_port_out']}" class="form-control" name="date_port_out" {$shipping_line_lock}>
+                </div>
+                <div class="col-6">
+                    <label>ACTUAL RECEIVED AT FALP</label>
+                    <input type="date" value="{$shipment['actual_received_at_falp']}" class="form-control" name="actual_received_at_falp">
+                </div>
+            </div>
+            <div class="row mb-2 d-flex align-items-center">
+                <div class="col-3 ml-auto">
+                    <button type="submit" class="btn bg-primary btn-block">Update</button>
+                </div>
+            </div>
+        HTML;
+    }
+
+    //polytainer details
+    $sql = "SELECT polytainer_size, polytainer_quantity, etd from m_polytainer_details where shipment_details_ref = :shipment_details_ref";
+    $stmt = $conn -> prepare($sql);
+    $stmt -> bindValue(':shipment_details_ref', $shipment_details_ref);
+    $stmt -> execute();
+    $shipment = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    if ($shipment) {
+        $shipment['etd'] = $shipment['etd'] == null ? null : substr($shipment['etd'], 0, 10);
+        $return_body['polytainer_details'] = <<<HTML
+            <input readonly style="display:none;" value="{$shipment_details_ref}" type="text" name="shipment_details_ref">
+            <div class="row mb-2">
+                <div class="col-6">
+                    <label>POLYTAINER SIZE</label>
+                    <input value="{$shipment['polytainer_size']}" type="text" class="form-control" name="polytainer_size">
+                </div>
+                <div class="col-6">
+                    <label>POLYTAINER QUANTITY</label>
+                    <input value="{$shipment['polytainer_quantity']}" type="text" class="form-control" name="polytainer_quantity">
+                </div>
+            </div>
+            <div class="row mb-2">
+                <div class="col-6">
+                    <label>ETD</label>
+                    <input value="{$shipment['etd']}" type="date" class="form-control" name="etd">
+                </div>
+            </div>
+            <div class="row mb-2 d-flex align-items-center">
+                <div class="col-3 ml-auto">
+                    <button type="submit" class="btn bg-primary btn-block">Update</button>
+                </div>
+            </div>
+        HTML;
+    }
+
+    //mm_system details
+    $sql = "SELECT container_status, date_return_reused, no_days_port, no_days_falp from m_mmsystem where shipment_details_ref = :shipment_details_ref";
+    $stmt = $conn -> prepare($sql);
+    $stmt -> bindValue(':shipment_details_ref', $shipment_details_ref);
+    $stmt -> execute();
+    $shipment = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    if ($shipment) {
+        $shipment['date_return_reused'] = $shipment['date_return_reused'] == null ? null : substr($shipment['date_return_reused'], 0, 10);
+        $return_body['mmsystem_details'] = <<<HTML
+            <input readonly style="display:none;" value="{$shipment_details_ref}" type="text" name="shipment_details_ref">
+            <div class="row mb-2">
+                <div class="col-6">
+                    <label>CONTAINER STATUS</label>
+                    <input value="{$shipment['container_status']}" type="text" class="form-control" name="container_status" {$shipping_line_lock}>
+                </div>
+                <div class="col-6">
+                    <label>DATE RETURN / REUSED</label>
+                    <input value="{$shipment['date_return_reused']}" type="date" class="form-control" name="date_return_reused" {$shipping_line_lock}>
+                </div>
+            </div>
+            <div class="row mb-2">
+                <div class="col-6">
+                    <label>NO. DAYS AT PORT</label>
+                    <input readonly value="{$shipment['no_days_port']}" type="number" class="form-control" name="no_days_port" {$shipping_line_lock}>
+                </div>
+                <div class="col-6">
+                    <label>NO. DAYS AT FALP</label>
+                    <input readonly value="{$shipment['no_days_falp']}" type="number" class="form-control" name="no_days_falp" {$shipping_line_lock}>
+                </div>
+            </div>
+            <div class="row mb-2 d-flex align-items-center">
+                <div class="col-3 ml-auto">
+                    <button type="submit" class="btn bg-primary btn-block">Update</button>
+                </div>
+            </div>
+        HTML;
+    }
+
+    //history details
+    $sql = "SELECT username, column_name, changed_from, changed_to, date_modified from m_change_history where shipment_details_ref = :shipment_details_ref order by date_modified asc";
+    $stmt = $conn -> prepare($sql);
+    $stmt -> bindValue(':shipment_details_ref', $shipment_details_ref);
+    $stmt -> execute();
+
+    $return_body['history'] = null;
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        $row['date_modified'] = date("Y/m/d", strtotime($row["date_modified"]));
+        $row['changed_from'] = $row['changed_from'] == null ? "N/A" : $row['changed_from'];
+        $row['changed_to'] = $row['changed_to'] == null ? "N/A" : $row['changed_to'];
+        $return_body['history'] .= <<<HTML
+            <tr>
+                <td>{$row['date_modified']}</td>
+                <td>{$row['username']}</td>
+                <td>{$row['column_name']}</td>
+                <td style="background-color: #ffcecb;">{$row['changed_from']}</td>
+                <td style="background-color: #d1f8d9;">{$row['changed_to']}</td>
+            </tr>
+        HTML;
+    }
+
+
+    
 
     //vessel_details
     $sql = "SELECT shipment_details_ref, vessel_name, eta_mnl, ata_mnl, atb from m_vessel_details where shipment_details_ref = :shipment_details_ref";
